bfbdcbadfb103c6417573298f072d965cca01d92
Jisoo Kim <jisoo000528@naver.com>
2024-03-21 14:33:37 +0900

    lru_sim.py updated

--- lru_sim/lru_sim.py
+++ lru_sim/lru_sim.py
@@ -1,14 +1,28 @@
-
-
 class CacheSimulator:
     def __init__(self, cache_slots):
         self.cache_slots = cache_slots
         self.cache_hit = 0
         self.tot_cnt = 1
+        self.cache = []
     
     def do_sim(self, page):
-        pass
-        # Do programming here! 
+        if len(self.cache) == 0:
+            self.cache.append(page)
+        else:
+            if len(self.cache) >= cache_slots:
+                if page in self.cache:
+                    self.cache.remove(page)
+                    self.cache_hit += 1
+                else:
+                    self.cache.pop(0)
+                self.cache.append(page)
+                self.tot_cnt += 1
+            else:
+                if page in self.cache:
+                    self.cache.remove(page)
+                    self.cache_hit += 1
+                self.cache.append(page)
+                self.tot_cnt += 1
         
     def print_stats(self):
         print("cache_slot = ", self.cache_slots, "cache_hit = ", self.cache_hit, "hit ratio = ", self.cache_hit / self.tot_cnt)
@@ -16,7 +30,7 @@ class CacheSimulator:
 
 if __name__ == "__main__":
 
-    data_file = open("./linkbench.trc")
+    data_file = open("C:\PYTHONPROJECT\자료구조\linkbench.trc")
     lines = data_file.readlines()
     for cache_slots in range(100, 1001, 100):
         cache_sim = CacheSimulator(cache_slots)
@@ -25,4 +39,3 @@ if __name__ == "__main__":
             cache_sim.do_sim(page)
         
         cache_sim.print_stats()
-
