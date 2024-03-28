07d2c68fbf1ac14adc227b4ae0513c8a9e70a927
Jisoo Kim <jisoo000528@naver.com>
2024-03-29 01:02:55 +0900

    lru_sim code added

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ds24
 자료구조 2024 재수강
+
+git remote -v
+
+git add *
+
+git commit -m "~"
+
+git push -u origin main
---
+++ lru_sim/lru_sim_list.py
@@ -0,0 +1,42 @@
+from list.linkedListBasic import *
+
+class CacheSimulator:
+	def __init__(self, cache_slots):
+		self.cache_slots = cache_slots
+		self.cache_hit = 0
+		self.tot_cnt = 1
+		self.lists = LinkedList()
+
+	def do_sim(self, page):
+		if self.lists.isEmpty():
+			self.lists.append(page)
+		else:
+			if self.lists.size() >= cache_slots:
+				if(self.lists.inNode(page)):
+					self.lists.remove(page)
+					self.cache_hit += 1
+				else:
+					self.lists.pop(0)
+				self.lists.append(page)
+				self.tot_cnt += 1
+			else:
+				if self.lists.inNode(page):
+					self.lists.remove(page)
+					self.cache_hit += 1
+				self.lists.append(page)
+				self.tot_cnt += 1
+
+	def print_stats(self):
+			print("cache_slot = ", self.cache_slots, "cache_hit = ", self.cache_hit, "hit ratio = ", self.cache_hit / self.tot_cnt)
+
+if __name__ == "__main__":
+
+	data_file = open("C:\PYTHONPROJECT\자료구조\linkbench.trc")
+	lines = data_file.readlines()
+	for cache_slots in range(100, 1001, 100):
+		cache_sim = CacheSimulator(cache_slots)
+		for line in lines:
+			page = line.split()[0]
+			cache_sim.do_sim(page)
+		
+		cache_sim.print_stats()
