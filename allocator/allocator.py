c94a29d9be9b2d421f2ca744f1750c8c0d91867a
Jisoo Kim <jisoo000528@naver.com>
2024-05-22 00:38:07 +0900

    allocator.py updated

--- allocator/allocator.py
+++ allocator/allocator.py
@@ -1,54 +1,86 @@
+class Node:
+	def __init__(self, size):
+		self.size = size
+		self.next = None
+
 class Allocator:
-    def __init__(self):
-        self.chunk_size = 4096
-        self.arena_size = 0
-        self.in_use = 0
-        self.free_space = 0
-        self.hash_table = ChainedHashTable(100000)
+	def __init__(self, chunk_size=4096):
+		self.chunk_size = chunk_size
+		self.arena = [None] * 100000
+		self.in_use_size = 0	# 사용 중인 메모리 크기
+		self.ac_num = 0			# 아레나 안에 있는 청크 개수
 
-    def print_stats(self):
-        print(f"Arena: {self.arena_size / (1024 * 1024):.2f} MB")
-        print(f"In-use: {self.in_use / (1024 * 1024):.2f} MB")
-        utilization = self.in_use / self.arena_size if self.arena_size != 0 else 0
-        print(f"Utilization: {utilization:.2f}")
+	def print_stats(self):
+		arena_size = self.ac_num * self.chunk_size
+		utilization = self.in_use_size / arena_size if arena_size > 0 else 0
 
-    def malloc(self, id, size):
-        if size % self.chunk_size != 0:
-            size = (size // self.chunk_size + 1) * self.chunk_size
-        if size > self.free_space:
-            additional_chunks = (size - self.free_space + self.chunk_size - 1) // self.chunk_size
-            self.arena_size += additional_chunks * self.chunk_size
-            self.free_space += additional_chunks * self.chunk_size
-        self.hash_table.insert(id, size)
-        self.in_use += size
-        self.free_space -= size
-        print(f"Allocated memory {id} of size {size} bytes")
+		# MB단위이므로 1M 나누기
+		print("Arena: ", arena_size // (1024 * 1024), "MB")
+		print("In-use: ", self.in_use_size // (1024 * 1024), "MB")
+		print("Utilization: {:.3f}".format(utilization))
+	
+	# chunk 사이즈에 맞게 해시테이블 노드 할당하기
+	def node_list(self, id, size, tNode:Node, n:int):
+		c_n = (size // self.chunk_size) + 1
+		if n == 1:
+			tNode.next = Node(size - c_n * self.chunk_size)
+		else:
+			tNode.next = Node(self.chunk_size)
+		n -= 1
+		if n != 0:
+			self.node_list(tNode.next, n)
 
-    def free(self, id):
-        size = self.hash_table.delete(id)
-        if size:
-            self.in_use -= size
-            self.free_space += size
-            print(f"Freed memory {id} of size {size} bytes")
-        else:
-            print(f"Error: Memory {id} not found")
 
+	def malloc(self, id, size):		# O(n)
+		# 할당해야할 청크 개수 구하고 해시 테이블 체인에 연결하기
+		chunk_num = (size // self.chunk_size) + 1
+		self.ac_num += chunk_num
+		if chunk_num > 1:
+			new_node = Node(self.chunk_size)
+			self.node_list(id, size, new_node, chunk_num - 1)
+		else:
+			new_node = Node(size)
+
+		if self.arena[id] is None:
+			self.arena[id] = new_node
+		else:
+			curr = self.arena[id]
+			while curr.next:
+				curr = curr.next
+			curr.next = new_node
+
+		self.in_use_size += size
+
+	def free(self, id):		# O(n)
+		curr = self.arena[id]
+
+		if curr is None:
+			print("free: No such ID in hash table")
+			return
+
+		# in_use 사이즈 업데이트
+		while curr:
+			self.in_use_size -= curr.size
+			curr = curr.next
+
+        # 해당 ID의 청크 모두 삭제
+		self.arena[id] = None
 
 if __name__ == "__main__":
-    allocator = Allocator()
-    
-    with open ("allocator/input.txt", "r") as file:
-        n=0
-        for line in file:
-            req = line.split()
-            if req[0] == 'a':
-                allocator.malloc(int(req[1]), int(req[2]))
-            elif req[0] == 'f':
-                allocator.free(int(req[1]))
+	allocator = Allocator()
 
-            # if n%100 == 0:
+	with open("allocator/input.txt", "r") as file:
+		n = 0
+		for line in file:
+			req = line.split()
+			if req[0] == 'a':
+				allocator.malloc(int(req[1]), int(req[2]))
+			elif req[0] == 'f':
+				allocator.free(int(req[1]))
+			
+			# if n%100 == 0:
             #     print(n, "...")
-            
-            n+=1
-    
-    allocator.print_stats()
+
+			n += 1
+
+	allocator.print_stats()
