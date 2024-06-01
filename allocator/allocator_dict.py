c3a396eb5804f77bf3807e039ff6dee006af3268
Jisoo Kim <jisoo000528@naver.com>
2024-06-02 01:06:17 +0900

    allocator_dict.py updated

--- allocator/allocator_dict.py
+++ allocator/allocator_dict.py
@@ -1,69 +1,73 @@
 class Allocator:
-    def __init__(self):
-        self.chunk_size = 4096
-        self.arena = {}
-        self.in_use_size = 0	# 사용 중인 메모리 크기
-        self.ac_num = 0	        # arena 안에 있는 청크 총 개수
-        
-    def print_stats(self):
-        arena_size = self.ac_num * self.chunk_size
-        utilization = self.in_use_size / arena_size
+	def __init__(self):
+		self.chunk_size = 4096
+		self.arena = {}
+		self.in_use_size = 0  # 사용 중인 메모리 크기
+		self.ac_num = 0  # arena 안에 있는 청크 총 개수
+		self.free_chunks = 0  # 사용 가능한 free space 청크 개수
 
-		# MB단위이므로 1M 나누기
-        print("Arena: ", arena_size // (1024 * 1024), "MB")     # 유효숫자 맞춰주기 위해서 '//' 사용함. 실제로 390.71875... 나옴
-        print("In-use: ", self.in_use_size // (1024 * 1024), "MB")      # 이하 동문. 실제로 162.18604...나옴
-        print("Utilization: {:.3f}".format(utilization))    # 유효숫자 맞추기 위해 3번째 소수점까지
+	def print_stats(self):
+		arena_size = self.ac_num * self.chunk_size
+		utilization = self.in_use_size / arena_size if arena_size > 0 else 0
 
-    def malloc(self, id, size):                     # 할당해야할 청크 개수 구하고 딕셔너리에 추가하기
-        chunk_num = (size // self.chunk_size) + 1
-        self.ac_num += chunk_num
-        if id in self.arena:                # arena에 이미 id가 존재하고 추가로 chunk 할당할 경우
-            if chunk_num == 1:
-                self.arena[id].append(size)
-            if chunk_num > 1:                          # chunk 개수가 2개 이상일 경우 dictionary에 chunk 리스트를 저장한다.
-                chunk_arr = [self.chunk_size] * (chunk_num - 1)
-                chunk_arr.append(size - (self.chunk_size * (chunk_num - 1)))
-                self.arena[id].extend(chunk_arr)
-        else:                               # 새로운 id에 메모리 할당할 경우
-            if chunk_num == 1:
-                self.arena[id] = [size]
-            if chunk_num > 1:                          # chunk 개수가 2개 이상일 경우 dictionary에 chunk 리스트를 저장한다.
-                chunk_arr = [self.chunk_size] * (chunk_num - 1)
-                chunk_arr.append(size - (self.chunk_size * (chunk_num - 1)))
-                self.arena[id] = chunk_arr
-        self.in_use_size += size
-    
-    def free(self, id):
-        if id not in self.arena:
-            print("free: No such ID in hash table")
-            return
+		print("Arena: ", arena_size // (1024 * 1024), "MB")  		# 유효숫자 맞춰주기 위해서 '//' 사용함. 실제로 390.71875... 나옴
+		print("In-use: ", self.in_use_size // (1024 * 1024), "MB")  # 이하 동문. 실제로 162.18604...나옴
+		print("Utilization: {:.3f}".format(utilization))  			# 유효숫자 맞추기 위해 3번째 소수점까지
 
-        if isinstance(self.arena[id], list):        # self.arena[id]의 type가 list인지 확인하는 조건문 -> true: chunk가 2개 이상
-            free_size = ((len(self.arena[id]) - 1) * 4096) + self.arena[id][len(self.arena[id]) - 1]
-            self.in_use_size -= free_size
-            self.ac_num -= len(self.arena[id])
-        else:                                       # chunk가 1개일 경우
-            self.in_use_size -= self.arena[id][0]
-            self.ac_num -= 1
+	def malloc(self, id, size):  # 할당해야할 청크 개수 구하고 딕셔너리에 추가하기
+		chunk_num = (size // self.chunk_size) + 1
 
-        del self.arena[id]
+		# Free space에서 필요한 청크를 할당
+		allocated_chunks = []
+		remaining_size = size
 
+		if self.free_chunks >= chunk_num:
+			self.free_chunks -= chunk_num
+		else:
+			chunk_num -= self.free_chunks
+			self.free_chunks = 0
+			self.ac_num += chunk_num
+
+		while remaining_size > self.chunk_size:
+			allocated_chunks.append(self.chunk_size)
+			remaining_size -= self.chunk_size
+
+		allocated_chunks.append(remaining_size)
+
+		if id in self.arena:  	# arena에 이미 id가 존재하고 추가로 chunk 할당할 경우
+			self.arena[id].extend(allocated_chunks)
+		else:  					# 새로운 id에 메모리 할당할 경우
+			self.arena[id] = allocated_chunks
+
+		self.in_use_size += size
+
+	def free(self, id):
+		if id not in self.arena:
+			print("free: No such ID in hash table")
+			return
+
+		free_chunks = self.arena[id]
+		chunk_count = len(free_chunks)
+
+		self.free_chunks += chunk_count
+		self.in_use_size -= sum(free_chunks)
+		del self.arena[id]
 
 if __name__ == "__main__":
-    allocator = Allocator()
-    
-    with open ("allocator/input.txt", "r") as file:
-        n=0
-        for line in file:
-            req = line.split()
-            if req[0] == 'a':
-                allocator.malloc(int(req[1]), int(req[2]))
-            elif req[0] == 'f':
-                allocator.free(int(req[1]))
+	allocator = Allocator()
 
-            # if n%100 == 0:
-            #     print(n, "...")
-            
-            n+=1
-    
-    allocator.print_stats()
+	with open("allocator/input.txt", "r") as file:
+		n = 0
+		for line in file:
+			req = line.split()
+			if req[0] == 'a':
+				allocator.malloc(int(req[1]), int(req[2]))
+			elif req[0] == 'f':
+				allocator.free(int(req[1]))
+			
+			# if n%100 == 0:
+			#     print(n, "...")
+
+			n += 1
+
+	allocator.print_stats()
