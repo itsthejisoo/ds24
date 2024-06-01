084d50774dc85c72f7ab483f2ebf545bcb0cb30d
Jisoo Kim <jisoo000528@naver.com>
2024-06-02 02:22:01 +0900

    allocator_dict.py updated

--- allocator/allocator_dict.py
+++ allocator/allocator_dict.py
@@ -14,44 +14,31 @@ class Allocator:
 		print("In-use: ", self.in_use_size // (1024 * 1024), "MB")  # 이하 동문. 실제로 162.18604...나옴
 		print("Utilization: {:.3f}".format(utilization))  			# 유효숫자 맞추기 위해 3번째 소수점까지
 
-	def malloc(self, id, size):  # 할당해야할 청크 개수 구하고 딕셔너리에 추가하기
-		chunk_num = (size // self.chunk_size) + 1
+	def malloc(self, id, size):
+		chunk_num = (size + self.chunk_size - 1) // self.chunk_size
 
-		# Free space에서 필요한 청크를 할당
-		allocated_chunks = []
-		remaining_size = size
-
-		if self.free_space >= chunk_num:	# free 청크가 충분한 경우
+		if self.free_space >= chunk_num:  	# free 청크가 충분한 경우
 			self.free_space -= chunk_num
-		else:								# free 청크가 충분하지 않은 경우
+		else:  								# free 청크가 충분하지 않은 경우
 			chunk_num -= self.free_space
-			self.free_space = 0
 			self.ac_num += chunk_num
+			self.free_space = 0
 
-		while remaining_size > self.chunk_size:
-			allocated_chunks.append(self.chunk_size)
-			remaining_size -= self.chunk_size
-
-		allocated_chunks.append(remaining_size)
-
-		if id in self.arena:  	# arena에 이미 id가 존재하고 추가로 chunk 할당할 경우
-			self.arena[id].extend(allocated_chunks)
-		else:  					# 새로운 id에 메모리 할당할 경우
-			self.arena[id] = allocated_chunks
-
-		self.in_use_size += size
+		if id not in self.arena:		# arena에 id값인 메모리가 처음으로 할당하는 경우
+			self.arena[id] = (size, chunk_num)
+			self.in_use_size += size
+		else:								# arena에 id값인 메모리가 이미 할당 되어 있는 경우
+			self.arena[id][0] += size
+			self.arena[id][1] += chunk_num
 
 	def free(self, id):
-		if id not in self.arena:
-			print("free: No such ID in hash table")
+		if id in self.arena:
+			size, chunk_num = self.arena[id]
+			self.free_space += chunk_num
+			self.in_use_size -= size
+		else:
+			print("free: No such ID in arena list")
 			return
-
-		free_space = self.arena[id]
-		chunk_count = len(free_space)
-
-		self.free_space += chunk_count
-		self.in_use_size -= sum(free_space)
-		del self.arena[id]
 
 if __name__ == "__main__":
 	allocator = Allocator()
